15359eb163df8314ecd7907e9d037c66d636bf70
sharvil31 <amburlesarvil@gmail.com>
2026-01-27 23:22:15 +0530

    Navigating and listing files (cd & ls)

---
+++ 2 Basics of Terminal/2 Navigating and listing files (cd & ls)/test.sh
@@ -0,0 +1,18 @@
+# go parent folder
+cd ../
+
+# change folder
+cd [folder_name]
+
+# jump from one folder to another 
+cd ../[folder name]
+
+
+# List content inside a directory
+ls
+
+# show all files including hidden 
+ls -a
+
+# show permissions 
+ls -la
