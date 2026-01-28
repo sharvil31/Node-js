8981a90b57f789a22f2aa01725550f7ab78e3cf6
sharvil31 <amburlesarvil@gmail.com>
2026-01-28 08:50:26 +0530

    What Is Prompt in Terminal

---
+++ 2 Basics of Terminal/4. Viewing and Editing Files/test.sh
@@ -0,0 +1,8 @@
+# Read file
+cat [file_name]
+
+# open file in Nano text editor
+nano [file_name]
+
+# open file in Vim text editor
+vim or vi [file_name]
---
+++ 2 Basics of Terminal/5 What Is Prompt in Terminal/test.sh
@@ -0,0 +1,7 @@
+# a prompt in terminal is a message or command that a computer system displays to indicate that it's ready to receive input or execute command
+
+# Change Prompt message string 1
+$PS1="Hello"
+
+# Change Prompt message string 2
+$PS2="World"
