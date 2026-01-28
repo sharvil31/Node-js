be73cc746f5c710f82d7c7fa90869eb5e5d9ab73
sharvil31 <amburlesarvil@gmail.com>
2026-01-28 09:03:21 +0530

    What Is Prompt in Terminal edited

--- 2 Basics of Terminal/5 What Is Prompt in Terminal/test.sh
+++ 2 Basics of Terminal/5 What Is Prompt in Terminal/test.sh
@@ -1,7 +1,9 @@
 # a prompt in terminal is a message or command that a computer system displays to indicate that it's ready to receive input or execute command
 
-# Change Prompt message string 1
+# Change Prompt message string 1 temporarily
 $PS1="Hello"
 
-# Change Prompt message string 2
+# Change Prompt message string 2 temporarily
 $PS2="World"
+
+# To change permanatly you can create .bashrc file and set there with same commands.
