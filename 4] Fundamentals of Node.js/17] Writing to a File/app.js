1d2c69780384785b976faf0b314c38734ff3975a
sharvil31 <amburlesarvil@gmail.com>
2026-06-13 08:31:20 +0530

    Writing to a File

--- 4] Fundamentals of Node.js/17] Writing to a File/app.js
+++ 4] Fundamentals of Node.js/17] Writing to a File/app.js
@@ -1,13 +1,13 @@
-import {appendFile, readFile, writeFile} from "node:fs/promises";
+import { appendFile, readFile, writeFile } from "node:fs/promises";
+import path from "node:path";
 
 // fs.writeFile("file-1.txt", "Hello World");
 
 // fs.appendFile("file-1.txt", "\nHello World");
 
-const contentBuffer = await readFile("./aot.jpg");
-console.log(contentBuffer);
-await writeFile("C:\\Users\\SHARVIL AMBURLE\\Desktop\\attackontitan.jpg", contentBuffer);
-
+// const contentBuffer = await readFile("./aot.jpg");
+// console.log(contentBuffer);
+// await writeFile("C:\\Users\\SHARVIL AMBURLE\\Desktop\\attackontitan.jpg", contentBuffer);
 
 // try {
 //     const contentBuffer = await readFile("apps.js");
@@ -17,3 +17,16 @@ await writeFile("C:\\Users\\SHARVIL AMBURLE\\Desktop\\attackontitan.jpg", conten
 //     console.log(error);
 //     console.log("To see full error message go to ./error.log file.");
 // }
+
+const [, , command, filename, copyPath] = process.argv;
+
+if (command === "copy") {
+  try {
+    const imgBuffer = await readFile(filename);
+    const fullPath = path.join(copyPath, path.basename(filename));
+    await writeFile(fullPath, imgBuffer);
+    console.log(`Copied successfully to: ${fullPath}`);
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+}
