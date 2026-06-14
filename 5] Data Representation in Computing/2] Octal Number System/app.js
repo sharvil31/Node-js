7b4e1c93f4e9b750105e56830848778137663947
sharvil31 <amburlesarvil@gmail.com>
2026-06-14 14:53:38 +0530

    Octal Number System

---
+++ 5] Data Representation in Computing/2] Octal Number System/app.js
@@ -0,0 +1,11 @@
+// const octalNum = 0o237
+const octalNum = 0o4527
+
+console.log(octalNum);
+
+const octaltoDecimal = 2 * Math.pow(8, 2) 
+                     + 3 * Math.pow(8, 1)
+                     + 7 * Math.pow(8, 0)
+
+// console.log(octaltoDecimal);
+
