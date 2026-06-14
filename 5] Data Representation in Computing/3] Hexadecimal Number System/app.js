dee1f739479a4261134d8745f18e5f7f3da18e31
sharvil31 <amburlesarvil@gmail.com>
2026-06-14 15:21:26 +0530

    Hexadecimal Number System

---
+++ 5] Data Representation in Computing/3] Hexadecimal Number System/app.js
@@ -0,0 +1,16 @@
+// const octalNum = 0o237
+
+// hexadecimal = {0 to 9, a, b, c, d, e, f}
+                        //  10  11 12 13 14 15
+
+const hexaNum = 0x237
+
+console.log(hexaNum);
+
+const hexaToDecimal = 2 * Math.pow(16, 2) 
+                     + 3 * Math.pow(16, 1)
+                     + 7 * Math.pow(16, 0)
+
+console.log(hexaToDecimal);
+console.log(parseInt(237, 8));
+
