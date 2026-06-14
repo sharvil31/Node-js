fbdccff43e491da3d0515628462484c37ee3b52b
sharvil31 <amburlesarvil@gmail.com>
2026-06-14 15:32:20 +0530

    Binary Number System

---
+++ 5] Data Representation in Computing/4] Binary Number System/app.js
@@ -0,0 +1,15 @@
+// const octalNum = 0o237
+
+// hexadecimal = {0 to 9, a, b, c, d, e, f}
+                        //  10  11 12 13 14 15
+
+const binNum = 0b10
+
+console.log(binNum);
+
+const binToDecimal = 1 * Math.pow(2, 0) 
+                     + 0 * Math.pow(2, 1)
+
+console.log(binToDecimal);
+console.log(parseInt("11", 2));
+
