dacac7be02c36c9d2fab064b1c1b3f02419eef25
sharvil31 <amburlesarvil@gmail.com>
2026-07-02 08:30:50 +0530

    Buffer Pool in Node.js

--- 6] Mastering Buffers/7] Buffer Pool in Node.js/app.js
+++ 6] Mastering Buffers/7] Buffer Pool in Node.js/app.js
@@ -3,11 +3,19 @@ import {Buffer} from "buffer";
 // Condition for allocUnsafe to use Buffer Pool
 // Buffer size < Buffer.poolSize >>> 2 (right shift - divide + floor) 
 
+Buffer.poolSize = 10000;
+
 const a = Buffer.alloc(4);
+const z = Buffer.alloc(4);
+
 // const b = Buffer.allocUnsafe(8);
 const b = Buffer.allocUnsafe(4095);
-const c = Buffer.allocUnsafe(4);
+// const c = Buffer.allocUnsafe(4095);
+const c = Buffer.allocUnsafe(4095 - 6);
+// const c = Buffer.allocUnsafe(4);
+const d = Buffer.from("abc"); // uses allocUnsafe bts
 
+const joinBuffer = Buffer.concat([a, z])
 b[2] = 97;
 c[0] = 100;
 
