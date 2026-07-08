321741305efcf7ca51af5edf73d5459294df9051
sharvil31 <amburlesarvil@gmail.com>
2026-07-08 22:16:28 +0530

    Different States of Readable Streams

--- 8] Node.js Streams - Handling Data Flow like a Pro/3] Different States of Readable Streams/app.js
+++ 8] Node.js Streams - Handling Data Flow like a Pro/3] Different States of Readable Streams/app.js
@@ -1,22 +1,43 @@
 import fs from "fs";
 
-const readStream = fs.createReadStream("chars.txt");
+const readStream = fs.createReadStream("chars.txt", { highWaterMark: 4 });
 
 readStream.on("data", (chunk) => {
-//   readStream.pause();
-//   console.log(readStream.readableFlowing);
-//   console.log(readStream.readableEnded);
-//   console.log(readStream.isPaused());
+  //   readStream.pause();
+  //   console.log(readStream.readableFlowing);
+  //   console.log(readStream.readableEnded);
+  //   console.log(readStream.isPaused());
 });
 
-readStream.on("end", () => {
-    console.log(readStream.readableEnded);
-    console.log(readStream.readableFlowing);
-    console.log(readStream.isPaused());
-});
+// readStream.on("end", () => {
+//     console.log(readStream.readableEnded);
+//     console.log(readStream.readableFlowing);
+//     console.log(readStream.isPaused());
+// });
 
 // console.log(readStream.readableFlowing);
 // console.log(readStream.readableEnded);
 // // readStream.pause();
 // console.log(readStream.isPaused());
 
+if (fs.readFileSync("abc.txt").byteLength) {
+  fs.writeFileSync("abc.txt", "");
+}
+
+readStream.on("data", (chunk) => {
+//   console.log(chunk);
+
+  fs.appendFileSync("abc.txt", chunk);
+//   readStream.pause();
+//   setTimeout(() => {
+//     readStream.resume();
+//   }, 500);
+});
+
+readStream.on("resume", () => {
+    console.log("Stream Resumed")
+})
+
+readStream.on("pause", () => {
+    console.log("Stream Paused")
+})
