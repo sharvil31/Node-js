12587f848a395e2782bb3b76e082f10b6c89eded
Sharvil Amburle <amburlesarvil@gmail.com>
2026-07-10 08:44:04 +0530

    Add files via upload

---
+++ 8] Node.js Streams - Handling Data Flow like a Pro/6] Writable Streams/backPressure.js
@@ -0,0 +1,27 @@
+// backpressure
+
+import fs from "fs";
+
+const writeStream = fs.createWriteStream("file.txt");
+
+console.time();
+const readStream = fs.createReadStream(
+  "E:\\[AO] Heaven s Feel I - Presage Flower [1080p] [Dual].mkv", // 2.14 gb file
+  { highWaterMark: 1 * 1024 * 1024 },
+);
+
+readStream.on("data", (chunkBuffer) => {
+  // time: 4-6s
+  // memory: 90-100mb
+  // CPU: max 12-17%
+  const isEmpty = writeStream.write(chunkBuffer);
+  if (!isEmpty) readStream.pause();
+});
+
+writeStream.on("drain", () => {
+  readStream.resume();
+});
+
+readStream.on("end", () => {
+  console.timeEnd();
+})
