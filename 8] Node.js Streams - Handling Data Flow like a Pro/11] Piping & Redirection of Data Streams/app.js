980c5fb9c2fc5aae3298e3cd4336f29d28f84660
root <root@DESKTOP-SHARVIL.>
2026-07-15 17:06:54 +0000

    Piping and Redirection of Data Streams

--- 8] Node.js Streams - Handling Data Flow like a Pro/11] Piping & Redirection of Data Streams/app.js
+++ 8] Node.js Streams - Handling Data Flow like a Pro/11] Piping & Redirection of Data Streams/app.js
@@ -1,1 +1,32 @@
+
 // Piping and Redirection of Data Streams
+
+// piping - connecting one process's stdout to another process's stdin.
+
+// file - app.js
+
+// echo "Hii" | node app.js - pipe only works in Ubuntu(Linux) terminal
+
+process.stdin.setEncoding("utf-8")
+process.stdin.on("data", (chunk) => {
+    console.log("app.js: ", chunk);
+})
+
+// another file - script.js
+
+// process.stdout.write("stdout: Hi from script.js\n")
+// process.stderr.write("stderr: Hi from script.js\n")
+// node script.js | node app.js
+
+// process.stderr.write("Hi from script.js\n")
+// node script.js | node app.js - pipe operator only pipes stdout 
+
+// Redirection - transfers one file's data to another file.
+
+// node script.js > output.txt
+// node script.js 2> output.txt
+// node script.js > output.txt 2> output.txt
+// node script.js > output.txt 2>> output.txt
+// node script.js >> output.txt 2>> output.txt
+
+// node app.js < output.txt
---
+++ 8] Node.js Streams - Handling Data Flow like a Pro/11] Piping & Redirection of Data Streams/script.js
@@ -0,0 +1,2 @@
+process.stdout.write("stdout: Hi from script.js\n")
+process.stderr.write("stderr: Hi from script.js\n")
