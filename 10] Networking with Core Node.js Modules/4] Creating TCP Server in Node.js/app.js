79c5f34f97a1c2e5306ef7b40c71c4ea7fbed408
sharvil31 <amburlesarvil@gmail.com>
2026-07-23 22:26:40 +0530

    Creating TCP Server in Node.js

---
+++ 10] Networking with Core Node.js Modules/4] Creating TCP Server in Node.js/app.js
@@ -0,0 +1,22 @@
+// Creating TCP Server in Node.js
+
+import net from "node:net"
+
+// Creating Server
+const server = net.createServer();
+
+// Starting a server
+// server.listen(4000); // Starts on host 0.0.0.0 and runs on all available IP addresses
+server.listen(4000, () => {
+    console.log("Server started on port 4000");
+});
+// OR use method
+//fires when server starts
+// server.on("listening", () => {
+//     console.log("Server started on port 4000");
+// });
+
+// new device connection
+server.on("connection", () => {
+    console.log("Client Connected");
+})
