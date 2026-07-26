94e7ee5ce0c0841d8189a4d82b6458d058a1fa80
sharvil31 <amburlesarvil@gmail.com>
2026-07-26 14:13:25 +0530

    Creating TCP Client using Node.js

--- 10] Networking with Core Node.js Modules/4] Creating TCP Server in Node.js/app.js
+++ 10] Networking with Core Node.js Modules/4] Creating TCP Server in Node.js/app.js
@@ -7,7 +7,7 @@ const server = net.createServer();
 
 // Starting a server
 // server.listen(4000); // Starts on host 0.0.0.0 and runs on all available IP addresses
-server.listen(4000, () => {
+server.listen(4000, "0.0.0.0", () => { // by default Ipv6
     console.log("Server started on port 4000");
 });
 // OR use method
@@ -20,7 +20,16 @@ server.listen(4000, () => {
 server.on("connection", (socket) => { // socket - duplex stream
     socket.on("data", (chunk) => {
         console.log(chunk.toString());
-    }) 
-    console.log(socket.address());
+        socket.end("HTTP\n\nGot your message"); // brower can understand tcp request because http is based on tcp
+    });
+
+    socket.on("close", () => {
+        console.log(socket.remoteAddress, ": Client Disconnected");
+    })
+
+    // console.log(socket.address());
+    console.log("Client Connected: ", socket.remoteAddress);
+    // console.log(socket.remotePort);
+    // console.log(socket.remoteFamily);
     console.log("Client Connected");
 })
