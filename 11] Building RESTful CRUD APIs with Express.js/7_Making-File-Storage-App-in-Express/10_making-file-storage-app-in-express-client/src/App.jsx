d844d698aa51de319aa62814c135590ce31859f5
sharvil31 <amburlesarvil@gmail.com>
2026-08-30 15:16:34 +0530

    Dynamic Routing in Express

--- 11] Building RESTful CRUD APIs with Express.js/7_Making-File-Storage-App-in-Express/10_making-file-storage-app-in-express-client/src/App.jsx
+++ 11] Building RESTful CRUD APIs with Express.js/7_Making-File-Storage-App-in-Express/10_making-file-storage-app-in-express-client/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const URL = "http://192.168.0.108:4000/";
+  const URL = "http://192.168.0.103:4000/";
   const [directoryItems, setDirectoryItems] = useState([]);
   const [progress, setProgress] = useState(0);
   const [newFilename, setNewFilename] = useState("");
@@ -33,9 +33,8 @@ function App() {
   }
 
   async function handleDelete(filename) {
-    const response = await fetch(URL, {
+    const response = await fetch(`${URL}${filename}`, {
       method: "DELETE",
-      body: filename,
     });
     const data = await response.text();
     console.log(data);
@@ -49,9 +48,12 @@ function App() {
 
   async function saveFilename(oldFilename) {
     setNewFilename(oldFilename);
-    const response = await fetch(URL, {
+    const response = await fetch(`${URL}${oldFilename}`, {
       method: "PATCH",
-      body: JSON.stringify({ oldFilename, newFilename }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ newFilename }),
     });
     const data = await response.text();
     console.log(data);
--- 11] Building RESTful CRUD APIs with Express.js/7_Making-File-Storage-App-in-Express/10_making-file-storage-app-in-express-server/app.js
+++ 11] Building RESTful CRUD APIs with Express.js/7_Making-File-Storage-App-in-Express/10_making-file-storage-app-in-express-server/app.js
@@ -1,24 +1,54 @@
 import express from "express";
-import { readdir } from "fs/promises";
+import { readdir, rename, rm } from "fs/promises";
 
 const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
-  res.set("Access-Control-Allow-Origin", "*");
+  res.set({
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "*",
+    "Access-Control-Allow-Headers": "*",
+  });
   next();
 });
 
-app.use((req, res, next) => {
-  if (req.query.action === "download") {
-    res.set("Content-Disposition", "attachment");
-  }
-  const serveStatic = express.static("storage");
-  serveStatic(req, res, next);
-}); // same as app.use(express.static("storage"))
-
+// Read
 app.get("/", async (req, res) => {
   const filesList = await readdir("./storage");
   res.json(filesList);
+});
+
+app.get("/:filename", (req, res) => {
+  const { filename } = req.params;
+  if (req.query.action === "download") {
+    res.set("Content-Disposition", "attachment");
+  }
+  res.sendFile(`${import.meta.dirname}/storage/${filename}`);
+});
+
+// Update
+app.patch("/:filename", async (req, res) => {
+  const { filename } = req.params;
+  try {
+    await rename(`./storage/${filename}`, `./storage/${req.body.newFilename}`);
+    res.json({message: "Renamed Successfully"});
+  } catch (error) {
+    res.json({ message: "Rename Failed!" });
+  }
+});
+
+// Delete
+app.delete("/:filename", async (req, res) => {
+  const { filename } = req.params;
+  const filePath = `./storage/${filename}`;
+  try {
+    await rm(filePath);
+    res.json({ message: "File Deleted Successfully" });
+  } catch (err) {
+    res.status(404).json({ message: "File Not Found!" });
+  }
 });
 
 app.listen(4000, () => {
