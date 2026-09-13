a5eb13581b25332ca65e8c69535a37f7551e5e59
sharvil31 <amburlesarvil@gmail.com>
2026-09-13 14:33:05 +0530

    Integrating VFS API in Our UI

--- 11] Building RESTful CRUD APIs with Express.js/8_Implementing-VFS-in-File-Storage-App/File-Storage-App-VPS-Client/src/DirectoryView.jsx
+++ 11] Building RESTful CRUD APIs with Express.js/8_Implementing-VFS-in-File-Storage-App/File-Storage-App-VPS-Client/src/DirectoryView.jsx
@@ -5,6 +5,8 @@ import { Link, useParams } from "react-router-dom";
 function DirectoryView() {
   const BASE_URL = "http://localhost:4000";
   const [directoryItems, setDirectoryItems] = useState([]);
+  const [directoriesList, setDirectoriesList] = useState([]);
+  const [filesList, setFilesList] = useState([]);
   const [progress, setProgress] = useState(0);
   const [newFilename, setNewFilename] = useState("");
   const [newDirName, setNewDirName] = useState("");
@@ -13,7 +15,8 @@ function DirectoryView() {
   async function getDirectoryItems() {
     const response = await fetch(`${BASE_URL}/directory/${dirPath}`);
     const data = await response.json();
-    setDirectoryItems(data);
+    setDirectoriesList(data.directories);
+    setFilesList(data.files);
   }
   useEffect(() => {
     getDirectoryItems();
@@ -22,7 +25,8 @@ function DirectoryView() {
   async function uploadFile(e) {
     const file = e.target.files[0];
     const xhr = new XMLHttpRequest();
-    xhr.open("POST", `${BASE_URL}/files/${dirPath}/${file.name}`, true);
+    xhr.open("POST", `${BASE_URL}/file/${file.name}`, true);
+    // xhr.setRequestHeader("parentdirid": null);
     xhr.addEventListener("load", () => {
       console.log(xhr.response);
       getDirectoryItems();
@@ -34,8 +38,8 @@ function DirectoryView() {
     xhr.send(file);
   }
 
-  async function handleDelete(filename) {
-    const response = await fetch(`${BASE_URL}/files/${dirPath}/${filename}`, {
+  async function handleDelete(id) {
+    const response = await fetch(`${BASE_URL}/file/${id}`, {
       method: "DELETE",
     });
     const data = await response.text();
@@ -48,18 +52,14 @@ function DirectoryView() {
     setNewFilename(oldFilename);
   }
 
-  async function saveFilename(oldFilename) {
-    setNewFilename(oldFilename);
-    const response = await fetch(
-      `${BASE_URL}/files/${dirPath}/${oldFilename}`,
-      {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ newFilename: `${dirPath}/${newFilename}` }),
+  async function saveFilename(id) {
+    const response = await fetch(`${BASE_URL}/file/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
       },
-    );
+      body: JSON.stringify({ newFilename }),
+    });
     const data = await response.text();
     console.log(data);
     setNewFilename("");
@@ -103,26 +103,15 @@ function DirectoryView() {
         <button>Create Directory</button>
       </form>
 
-      {directoryItems.map(({ name, isDirectory }, i) => (
-        <div key={i}>
-          {name}{" "}
-          {isDirectory ? (
-            <Link to={`./${name}`}>Open </Link>
-          ) : (
-            <a href={`${BASE_URL}/files/${dirPath}/${name}?action=open`}>
-              Open{" "}
-            </a>
-          )}
-          {!isDirectory && (
-            <a href={`${BASE_URL}/files/${dirPath}/${name}?action=download`}>
-              Download
-            </a>
-          )}
+      {filesList.map(({ name, id }, i) => (
+        <div key={id}>
+          {name} <a href={`${BASE_URL}/file/${id}`}>Open </a>
+          <a href={`${BASE_URL}/file/${id}?action=download`}>Download</a>
           <button onClick={() => renameFile(name)}>Rename</button>
-          <button onClick={() => saveFilename(name)}>Save</button>
+          <button onClick={() => saveFilename(id)}>Save</button>
           <button
             onClick={() => {
-              handleDelete(name);
+              handleDelete(id);
             }}
           >
             Delete
--- 11] Building RESTful CRUD APIs with Express.js/8_Implementing-VFS-in-File-Storage-App/File-Storage-App-VPS-Server/routes/fileRoutes.js
+++ 11] Building RESTful CRUD APIs with Express.js/8_Implementing-VFS-in-File-Storage-App/File-Storage-App-VPS-Server/routes/fileRoutes.js
@@ -39,10 +39,10 @@ router.get("/:id", (req, res) => {
   const fileData = filesData.find((file) => file.id === id);
   console.log(fileData);
   if (req.query.action === "download") {
-    res.set("Content-Disposition", "attachment");
+    res.set("Content-Disposition", `attachment; filename=${fileData.name}`);
   }
   res.sendFile(`${process.cwd()}/storage/${id}${fileData.extension}`, (err) => {
-    if (err) res.json({ error: "File Not Found!" });
+    if (!res.headersSent) res.json({ error: "File Not Found!" });
   });
 });
 
